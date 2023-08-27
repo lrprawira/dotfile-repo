2e8e36a1ebb78b395bc4d6c43a6ddcbcc4a8a370
Louis Raymond <louis@quintal.id>
2023-08-27 22:21:10 +0700

    Reconfigure mypy as lvim python linter

--- XDG_CONFIG_HOME/lvim/config.lua
+++ XDG_CONFIG_HOME/lvim/config.lua
@@ -3,7 +3,6 @@
   `lvim` is the global options object
  ]]
 -- vim options
--- vim.opt.expandtab = true
 vim.opt.expandtab = false
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
@@ -13,9 +12,9 @@ vim.opt.shell = "/bin/bash"
 -- general
 lvim.log.level = "info"
 lvim.format_on_save = {
-  enabled = true,
-  pattern = "*.lua",
-  timeout = 1000,
+	enabled = true,
+	pattern = "*.lua",
+	timeout = 1000,
 }
 
 -- to disable icons and use a minimalist setup, uncomment the following
@@ -64,18 +63,18 @@ lvim.builtin.treesitter.auto_install = true
 -- ---remove a server from the skipped list, e.g. eslint, or emmet_ls. IMPORTANT: Requires `:LvimCacheReset` to take effect
 -- ---`:LvimInfo` lists which server(s) are skipped for the current filetype
 require("lvim.lsp.manager").setup("pyright", {
-  settings = {
-    python = {
-      analysis = {
-        typeCheckingMode = "off"
-      }
-    }
-  }
+	settings = {
+		python = {
+			analysis = {
+				typeCheckingMode = "off"
+			}
+		}
+	}
 })
-vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "tsserver" })
-lvim.lsp.automatic_configuration.skipped_servers = vim.tbl_filter(function(server)
-  return server ~= "rome"
-end, lvim.lsp.automatic_configuration.skipped_servers)
+-- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "tsserver" })
+-- lvim.lsp.automatic_configuration.skipped_servers = vim.tbl_filter(function(server)
+--   return server ~= "rome"
+-- end, lvim.lsp.automatic_configuration.skipped_servers)
 
 -- -- you can set a custom on_attach function that will be used for all the language servers
 -- -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
@@ -91,8 +90,8 @@ end, lvim.lsp.automatic_configuration.skipped_servers)
 -- local formatters = require "lvim.lsp.null-ls.formatters"
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
-  { name = "blue",  filetypes = { "python" } },
-  { name = "djlint" },
+	{ name = "blue",  filetypes = { "python" } },
+	{ name = "djlint" },
 }
 -- formatters.setup {
 --   { command = "stylua" },
@@ -102,190 +101,190 @@ formatters.setup {
 --     filetypes = { "typescript", "typescriptreact" },
 --   },
 -- }
--- local linters = require "lvim.lsp.null-ls.linters"
 local linters = require "lvim.lsp.null-ls.linters"
 linters.setup {
-  { name = "djlint", filetypes = { "html", "django-html", "htmldjango" } },
+	{ name = "djlint",  filetypes = { "html", "django-html", "htmldjango" } },
+	{ command = "mypy", filetypes = { "python" } },
+	{
+		command = "shellcheck",
+		args = { "--severity", "warning" },
+	},
 }
--- linters.setup {
---   { command = "flake8", filetypes = { "python" } },
---   {
---     command = "shellcheck",
---     args = { "--severity", "warning" },
---   },
--- }
 
 -- -- Additional Plugins <https://www.lunarvim.org/docs/plugins#user-plugins>
 lvim.plugins = {
-  -- {
-  --   "folke/trouble.nvim",
-  --   cmd = "TroubleToggle",
-  -- },
-  {
-    "folke/persistence.nvim",
-    event = "BufReadPre",
-    persistence = "persistence",
-    config = function()
-      require("persistence").setup({
-        dir = vim.fn.expand(vim.fn.stdpath "state" .. "/sessions/"),
-        options = { "buffers", "curdir", "tabpages", "winsize" },
-      })
-    end,
-  },
-  {
-    "folke/todo-comments.nvim",
-    event = "BufRead",
-    config = function()
-      require("todo-comments").setup()
-    end,
-  },
-  {
-    "Pocco81/auto-save.nvim",
-    event = "BufRead",
-    config = function()
-      require("auto-save").setup()
-    end,
-  },
-  -- {
-  --   "karb94/neoscroll.nvim",
-  --   event = "WinScrolled",
-  --   config = function()
-  --     require('neoscroll').setup({
-  --       -- All these keys will be mapped to their corresponding default scrolling animation
-  --       mappings = { '<C-u>', '<C-d>', '<C-b>', '<C-f>',
-  --         '<C-y>', '<C-e>', 'zt', 'zz', 'zb' },
-  --       hide_cursor = true,          -- Hide cursor while scrolling
-  --       stop_eof = true,             -- Stop at <EOF> when scrolling downwards
-  --       use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
-  --       respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
-  --       cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
-  --       easing_function = nil,       -- Default easing function
-  --       pre_hook = nil,              -- Function to run before the scrolling animation starts
-  --       post_hook = nil,             -- Function to run after the scrolling animation ends
-  --     })
-  --   end
-  -- },
-  {
-    "ethanholz/nvim-lastplace",
-    event = "BufRead",
-    config = function()
-      require("nvim-lastplace").setup({
-        lastplace_ignore_buftype = { "quickfix", "nofile", "help" },
-        lastplace_ignore_filetype = {
-          "gitcommit", "gitrebase", "svn", "hgcommit",
-        },
-        lastplace_open_folds = true,
-      })
-    end,
-  },
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      require("catppuccin").setup({
-        flavour = "macchiato", -- latte, frappe, mocha, macchiato
-        -- background = {
-        --   light = "latte",
-        --   dark = "frappe",
-        -- },
-        -- transparent_background: false,
-        show_end_of_buffer = true,
-        term_colors = false,
-        integrations = {
-          aerial = true
-        }
-      })
-    end
-  },
-  -- {
-  --   "RRethy/nvim-base16",
-  --   priority = 1000,
-  -- },
-  -- {
-  --   "wfxr/minimap.vim",
-  --   config = function()
-  --     vim.cmd("let g:minimap_width = 10")
-  --     vim.cmd("let g:minimap_auto_start = 1")
-  --     vim.cmd("let g:minimap_auto_start_win_enter = 1")
-  --   end
-  -- },
-  {
-    "windwp/nvim-ts-autotag",
-    event = "BufRead",
-    config = function()
-      require("nvim-ts-autotag").setup()
-    end
-  },
-  {
-    "mrjones2014/nvim-ts-rainbow",
-    event = "BufReadPost",
-  },
-  {
-    "norcalli/nvim-colorizer.lua",
-    event = "BufReadPre",
-    config = function()
-      require('colorizer').setup()
-    end
-  },
-  {
-    "stevearc/aerial.nvim",
-    opts = {},
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "nvim-tree/nvim-web-devicons"
-    },
-    cmd = { "AerialToggle", "AerialOpen", "AerialOpenAll", "AerialNavOpen", "AerialNavToggle" },
-    config = function()
-      require('aerial').setup({
-        layout = {
-          min_width = 20,
-        },
-        on_attach = function(bufnr)
-          vim.keymap.set('n', '{', '<cmd>AerialPrev<CR>', { buffer = bufnr })
-          vim.keymap.set('n', '}', '<cmd>AerialNext<CR>', { buffer = bufnr })
-        end
-      })
-    end,
-  },
-  {
-    "ggandor/leap.nvim",
-    name = "leap",
-    config = function()
-      require("leap")
-          .add_default_mappings()
-      -- require("leap")
-      --     .add_repeat_mappings(';', ',', {
-      --       relative_directions = true,
-      --       modes = { 'n', 'x', 'o' },
-      --     })
-    end,
-  },
-  -- {
-  --   "ggandor/flit.nvim",
-  --   name = "flit",
-  --   dependencies = { "leap" },
-  --   config = function()
-  --     require("flit").setup({
-  --       labeled_modes = "nvo",
-  --     })
-  --   end
-  -- },
-  {
-    "ray-x/lsp_signature.nvim",
-    event = "BufRead",
-    config = function()
-      require("lsp_signature").on_attach()
-    end
-  },
-  {
-    "nvim-orgmode/orgmode",
-    config = function()
-      local orgmode = require('orgmode')
-      orgmode.setup()
-      orgmode.setup_ts_grammar()
-    end
-  },
+	-- {
+	--   "folke/trouble.nvim",
+	--   cmd = "TroubleToggle",
+	-- },
+	{
+		"folke/persistence.nvim",
+		event = "BufReadPre",
+		persistence = "persistence",
+		config = function()
+			require("persistence").setup({
+				dir = vim.fn.expand(vim.fn.stdpath "state" .. "/sessions/"),
+				options = { "buffers", "curdir", "tabpages", "winsize" },
+			})
+		end,
+	},
+	{
+		"folke/todo-comments.nvim",
+		event = "BufRead",
+		config = function()
+			require("todo-comments").setup()
+		end,
+	},
+	{
+		"Pocco81/auto-save.nvim",
+		event = "BufRead",
+		config = function()
+			require("auto-save").setup()
+		end,
+	},
+	{
+		"rmagatti/goto-preview",
+		config = function()
+			require('goto-preview').setup({
+				width = 120,
+				height = 25,
+				default_mappings = false,
+				debug = false,
+				opacity = nil,
+				post_open_hook = nil,
+			})
+		end
+	},
+	{ "nvim-treesitter/nvim-treesitter-textobjects", event = "BufRead" },
+	-- {
+	-- 	"kylechui/nvim-surround",
+	-- 	version = "*",
+	-- 	event = "VeryLazy",
+	-- 	config = function()
+	-- 		require('nvim-surround').setup({})
+	-- 	end
+	-- },
+	{
+		"ethanholz/nvim-lastplace",
+		event = "BufRead",
+		config = function()
+			require("nvim-lastplace").setup({
+				lastplace_ignore_buftype = { "quickfix", "nofile", "help" },
+				lastplace_ignore_filetype = {
+					"gitcommit", "gitrebase", "svn", "hgcommit",
+				},
+				lastplace_open_folds = true,
+			})
+		end,
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				flavour = "macchiato", -- latte, frappe, mocha, macchiato
+				-- background = {
+				--   light = "latte",
+				--   dark = "frappe",
+				-- },
+				-- transparent_background: false,
+				show_end_of_buffer = true,
+				term_colors = false,
+				integrations = {
+					aerial = true
+				}
+			})
+		end
+	},
+	-- {
+	--   "RRethy/nvim-base16",
+	--   priority = 1000,
+	-- },
+	-- {
+	--   "wfxr/minimap.vim",
+	--   config = function()
+	--     vim.cmd("let g:minimap_width = 10")
+	--     vim.cmd("let g:minimap_auto_start = 1")
+	--     vim.cmd("let g:minimap_auto_start_win_enter = 1")
+	--   end
+	-- },
+	{
+		"windwp/nvim-ts-autotag",
+		event = "BufRead",
+		config = function()
+			require("nvim-ts-autotag").setup()
+		end
+	},
+	{
+		"mrjones2014/nvim-ts-rainbow",
+		event = "BufReadPost",
+	},
+	{
+		"norcalli/nvim-colorizer.lua",
+		event = "BufReadPre",
+		config = function()
+			require('colorizer').setup()
+		end
+	},
+	{
+		"stevearc/aerial.nvim",
+		opts = {},
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-tree/nvim-web-devicons"
+		},
+		cmd = { "AerialToggle", "AerialOpen", "AerialOpenAll", "AerialNavOpen", "AerialNavToggle" },
+		config = function()
+			require('aerial').setup({
+				layout = {
+					min_width = 20,
+				},
+				on_attach = function(bufnr)
+					vim.keymap.set('n', '{', '<cmd>AerialPrev<CR>', { buffer = bufnr })
+					vim.keymap.set('n', '}', '<cmd>AerialNext<CR>', { buffer = bufnr })
+				end
+			})
+		end,
+	},
+	{
+		"ggandor/leap.nvim",
+		name = "leap",
+		config = function()
+			require("leap")
+					.add_default_mappings()
+			-- require("leap")
+			--     .add_repeat_mappings(';', ',', {
+			--       relative_directions = true,
+			--       modes = { 'n', 'x', 'o' },
+			--     })
+		end,
+	},
+	-- {
+	--   "ggandor/flit.nvim",
+	--   name = "flit",
+	--   dependencies = { "leap" },
+	--   config = function()
+	--     require("flit").setup({
+	--       labeled_modes = "nvo",
+	--     })
+	--   end
+	-- },
+	{
+		"ray-x/lsp_signature.nvim",
+		event = "BufRead",
+		config = function()
+			require("lsp_signature").on_attach()
+		end
+	},
+	{
+		"nvim-orgmode/orgmode",
+		config = function()
+			local orgmode = require('orgmode')
+			orgmode.setup()
+			orgmode.setup_ts_grammar()
+		end
+	},
 }
 
 --
@@ -304,10 +303,10 @@ lvim.builtin.mason.max_concurrent_installers = 8
 -- lvim.builtin.cmp.sources = { {} }
 -- Persistence
 lvim.builtin.which_key.mappings["S"] = {
-  name = "Session",
-  c = { "<cmd>lua require('persistence').load()<cr>", "Restore last session for current dir" },
-  l = { "<cmd>lua require('persistence').load({ last = true })<cr>", "Restore last session" },
-  Q = { "<cmd>lua require('persistence').stop()<cr>", "Quit without saving session" },
+	name = "Session",
+	c = { "<cmd>lua require('persistence').load()<cr>", "Restore last session for current dir" },
+	l = { "<cmd>lua require('persistence').load({ last = true })<cr>", "Restore last session" },
+	Q = { "<cmd>lua require('persistence').stop()<cr>", "Quit without saving session" },
 }
 -- Bufferline
 lvim.builtin.bufferline.after = "catppuccin"
