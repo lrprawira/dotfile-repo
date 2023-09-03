a44cb06f13d210f34c9b3479e2b5b4ba6efaabe0
Louis Raymond <ilikebikes+gitlab-verbosity@femmund.com>
2023-09-03 14:02:29 +0700

    Setup lvim nvim-surround keybindings

--- XDG_CONFIG_HOME/lvim/config.lua
+++ XDG_CONFIG_HOME/lvim/config.lua
@@ -250,7 +250,20 @@ lvim.plugins = {
 		version = "*",
 		event = "VeryLazy",
 		config = function()
-			-- 		require('nvim-surround').setup({})
+			require('nvim-surround').setup({
+				keymaps = {
+					-- insert          = '<C-g>z',
+					-- insert_line     = '<C-g>Z',
+					normal          = 'gz',
+					normal_cur      = 'gZ',
+					normal_line     = 'gzgz',
+					normal_cur_line = 'gZgZ',
+					visual          = 'gz',
+					visual_line     = 'gZ',
+					delete          = 'gzd',
+					change          = 'gzr',
+				}
+			})
 		end
 	},
 	{
