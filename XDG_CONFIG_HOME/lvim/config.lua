81e4d1bf5e5b2784d4f4df8e7babcd88040ee927
Louis Raymond <louis@quintal.id>
2023-09-09 11:36:56 +0700

    Add neorg to lvim

--- XDG_CONFIG_HOME/lvim/config.lua
+++ XDG_CONFIG_HOME/lvim/config.lua
@@ -363,6 +363,26 @@ lvim.plugins = {
 			require("lsp_signature").on_attach()
 		end
 	},
+	-- {
+	-- 	"nvim-neorg/neorg",
+	-- 	build = ":Neorg sync-parsers",
+	-- 	dependencies = { "nvim-lua/plenary.nvim" },
+	-- 	config = function()
+	-- 		require("neorg").setup {
+	-- 			load = {
+	-- 				["core.defaults"] = {},  -- Loads default behaviour
+	-- 				["core.concealer"] = {}, -- Adds pretty icons to your documents
+	-- 				["core.dirman"] = {      -- Manages Neorg workspaces
+	-- 					config = {
+	-- 						workspaces = {
+	-- 							notes = "~/Notes",
+	-- 						},
+	-- 					},
+	-- 				},
+	-- 			},
+	-- 		}
+	-- 	end,
+	-- },
 	{
 		'tpope/vim-dadbod',
 	},
