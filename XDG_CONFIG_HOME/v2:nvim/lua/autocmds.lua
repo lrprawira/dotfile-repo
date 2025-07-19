7f24db60838417ef581a320e8a7cfae06690f1e0
Louis Raymond <ilikebikes+gitlab-verbosity@femmund.com>
2025-07-19 18:01:53 +0700

    Fix error on vimleave uv__is_closing assertion error workaround

--- XDG_CONFIG_HOME/v2:nvim/lua/autocmds.lua
+++ XDG_CONFIG_HOME/v2:nvim/lua/autocmds.lua
@@ -3,3 +3,11 @@ vim.api.nvim_create_autocmd('BufReadPre', {
     local ft = vim.bo.filetype
   end
 })
+
+-- Workaround uv__is_closing assertion error
+vim.api.nvim_create_autocmd({ "VimLeave" }, {
+  callback = function()
+    vim.cmd("!notify-send ''")
+    vim.cmd("sleep 10m")
+  end,
+})
